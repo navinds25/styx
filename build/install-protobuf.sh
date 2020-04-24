4846a8e10287b561636f42e90a58c0e138926c27
Navin Sadanandan <navinds25@gmail.com>
2020-04-24 14:00:24 +0530

    travis debugging

--- build/install-protobuf.sh
+++ build/install-protobuf.sh
@@ -6,4 +6,4 @@ wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/proto
 unzip protoc-3.11.4-linux-x86_64.zip
 sudo cp -r include/* /usr/bin/include/
 sudo cp -r bin/* /usr/bin/
-go get github.com/golang/protobuf@v1.4.0
+#go get github.com/golang/protobuf@v1.4.0
