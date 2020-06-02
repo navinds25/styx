eece541773116f8f25316dafafef92e674455192
Navin Sadanandan <navinds25@gmail.com>
2020-06-02 17:31:13 +0530

    changed extension to extension stream

--- api/extension/extension.pb.go
+++ api/extension/extension.pb.go
@@ -118,7 +118,7 @@ func init() {
 func init() { proto.RegisterFile("extension.proto", fileDescriptor_2d065b70573ae483) }
 
 var fileDescriptor_2d065b70573ae483 = []byte{
-	// 241 bytes of a gzipped FileDescriptorProto
+	// 243 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4f, 0xad, 0x28, 0x49,
 	0xcd, 0x2b, 0xce, 0xcc, 0xcf, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x84, 0x0b, 0x28,
 	0xed, 0x62, 0xe4, 0x12, 0x73, 0xad, 0x48, 0x4d, 0x2e, 0x2d, 0x49, 0x75, 0x85, 0x09, 0x06, 0x24,
@@ -130,11 +130,11 @@ var fileDescriptor_2d065b70573ae483 = []byte{
 	0x5a, 0x96, 0x98, 0x53, 0x9a, 0x2a, 0xc1, 0x04, 0x16, 0x83, 0x70, 0xac, 0x98, 0x2c, 0x18, 0x95,
 	0x66, 0x61, 0x71, 0xbc, 0x7f, 0x69, 0x49, 0x41, 0x69, 0x89, 0x90, 0x2b, 0x17, 0x5b, 0x3e, 0x98,
 	0x25, 0xc1, 0x08, 0x76, 0xa5, 0x2e, 0x1e, 0x57, 0x42, 0xb4, 0xe8, 0x41, 0x28, 0x88, 0x13, 0xa1,
-	0x9a, 0xa5, 0x2c, 0xb9, 0xb8, 0x91, 0x84, 0x49, 0x71, 0x9c, 0x51, 0x0e, 0x97, 0x00, 0xdc, 0x86,
-	0xe0, 0xd4, 0xa2, 0xb2, 0xcc, 0xe4, 0x54, 0xa1, 0x08, 0x90, 0x18, 0xaa, 0xe5, 0x42, 0x8a, 0x04,
-	0xc3, 0x4f, 0x4a, 0x91, 0xa0, 0xe3, 0x9d, 0x78, 0xa3, 0xb8, 0xf5, 0xac, 0xe1, 0xaa, 0x92, 0xd8,
-	0xc0, 0x11, 0x6d, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x21, 0xff, 0x67, 0x14, 0xfb, 0x01, 0x00,
-	0x00,
+	0x9a, 0xa5, 0x2c, 0xb9, 0xb8, 0x91, 0x84, 0x49, 0x71, 0x9c, 0x51, 0x1e, 0x97, 0x00, 0xdc, 0x86,
+	0xe0, 0xd4, 0xa2, 0xb2, 0xcc, 0xe4, 0x54, 0xa1, 0x28, 0x90, 0x18, 0xaa, 0xe5, 0x42, 0x8a, 0x04,
+	0xc3, 0x4f, 0x4a, 0x91, 0xa0, 0xe3, 0x0d, 0x18, 0x9d, 0x78, 0xa3, 0xb8, 0xf5, 0xac, 0xe1, 0xea,
+	0x92, 0xd8, 0xc0, 0x51, 0x6d, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x6b, 0x5e, 0x26, 0xb1, 0xfd,
+	0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -149,7 +149,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ExtensionServiceClient interface {
-	ExecuteExtension(ctx context.Context, in *ExecuteExtensionParams, opts ...grpc.CallOption) (*ExecuteExtensionOutput, error)
+	ExecuteExtension(ctx context.Context, in *ExecuteExtensionParams, opts ...grpc.CallOption) (ExtensionService_ExecuteExtensionClient, error)
 }
 
 type extensionServiceClient struct {
@@ -160,51 +160,78 @@ func NewExtensionServiceClient(cc *grpc.ClientConn) ExtensionServiceClient {
 	return &extensionServiceClient{cc}
 }
 
-func (c *extensionServiceClient) ExecuteExtension(ctx context.Context, in *ExecuteExtensionParams, opts ...grpc.CallOption) (*ExecuteExtensionOutput, error) {
-	out := new(ExecuteExtensionOutput)
-	err := c.cc.Invoke(ctx, "/extension.ExtensionService/ExecuteExtension", in, out, opts...)
+func (c *extensionServiceClient) ExecuteExtension(ctx context.Context, in *ExecuteExtensionParams, opts ...grpc.CallOption) (ExtensionService_ExecuteExtensionClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_ExtensionService_serviceDesc.Streams[0], "/extension.ExtensionService/ExecuteExtension", opts...)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	x := &extensionServiceExecuteExtensionClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type ExtensionService_ExecuteExtensionClient interface {
+	Recv() (*ExecuteExtensionOutput, error)
+	grpc.ClientStream
+}
+
+type extensionServiceExecuteExtensionClient struct {
+	grpc.ClientStream
+}
+
+func (x *extensionServiceExecuteExtensionClient) Recv() (*ExecuteExtensionOutput, error) {
+	m := new(ExecuteExtensionOutput)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 // ExtensionServiceServer is the server API for ExtensionService service.
 type ExtensionServiceServer interface {
-	ExecuteExtension(context.Context, *ExecuteExtensionParams) (*ExecuteExtensionOutput, error)
+	ExecuteExtension(*ExecuteExtensionParams, ExtensionService_ExecuteExtensionServer) error
 }
 
 func RegisterExtensionServiceServer(s *grpc.Server, srv ExtensionServiceServer) {
 	s.RegisterService(&_ExtensionService_serviceDesc, srv)
 }
 
-func _ExtensionService_ExecuteExtension_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ExecuteExtensionParams)
-	if err := dec(in); err != nil {
-		return nil, err
+func _ExtensionService_ExecuteExtension_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(ExecuteExtensionParams)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
 	}
-	if interceptor == nil {
-		return srv.(ExtensionServiceServer).ExecuteExtension(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/extension.ExtensionService/ExecuteExtension",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ExtensionServiceServer).ExecuteExtension(ctx, req.(*ExecuteExtensionParams))
-	}
-	return interceptor(ctx, in, info, handler)
+	return srv.(ExtensionServiceServer).ExecuteExtension(m, &extensionServiceExecuteExtensionServer{stream})
+}
+
+type ExtensionService_ExecuteExtensionServer interface {
+	Send(*ExecuteExtensionOutput) error
+	grpc.ServerStream
+}
+
+type extensionServiceExecuteExtensionServer struct {
+	grpc.ServerStream
+}
+
+func (x *extensionServiceExecuteExtensionServer) Send(m *ExecuteExtensionOutput) error {
+	return x.ServerStream.SendMsg(m)
 }
 
 var _ExtensionService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "extension.ExtensionService",
 	HandlerType: (*ExtensionServiceServer)(nil),
-	Methods: []grpc.MethodDesc{
+	Methods:     []grpc.MethodDesc{},
+	Streams: []grpc.StreamDesc{
 		{
-			MethodName: "ExecuteExtension",
-			Handler:    _ExtensionService_ExecuteExtension_Handler,
+			StreamName:    "ExecuteExtension",
+			Handler:       _ExtensionService_ExecuteExtension_Handler,
+			ServerStreams: true,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
 	Metadata: "extension.proto",
 }
